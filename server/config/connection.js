0d08bdcf356dd4538edb62f4792e21ce4510307a
Juanita Samborski <juanita@breakthroughwebsite.com>
2023-07-12 20:57:39 -0700

    MongoDB updates.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/teamDB",
-  {
+mongoose
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }
-);
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
-module.exports = mongoose.connection;
+const db = mongoose.connection;
+module.exports = db;
+
